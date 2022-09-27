f5b2065890040b4d8755d6927143a00948eab165
sally <peacheeks@gmail.com>
2022-09-27 21:28:10 +0900

    search detail

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,9 +1,70 @@
 import React from 'react'
+import './search.css';
+import axios from '../../api/axios';
+import { useState, useEffect  } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function SearchPage() {
-  return (
-    <div>SearchPage</div>
-  )
+
+  const [searchResults, setSearchResults] = useState([])
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+
+  let query = useQuery();
+  const searchTerm = query.get('q');
+  console.log('searchTerm', searchTerm);
+  
+  useEffect(() => {
+    if(searchTerm){
+      fetchSearchMovie(searchTerm);
+    }
+  }  , [searchTerm])
+
+  const fetchSearchMovie = async () => { 
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`)
+    
+    console.log(request);
+    setSearchResults(request.data.results);
+      } catch (error) {
+        console.log(error);
+      }     
 }
 
-export default SearchPage
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? (
+      <section className='search-container'>
+        {searchResults.map((movie) => {
+          if(movie.backdrop_path !== null && movie.media_type !== 'person'){
+            const movieImageUrl = 
+            'https://image.tmdb.org/t/p/w500' + movie.backdrop_path
+            return (
+              <div className='movie'>
+                <div
+                  className='movie__column-poster'
+                >
+                  <img
+                    src={movieImageUrl} alt='movieimage'
+                    className='movie__poster' />
+                </div>
+              </div>
+            )
+          }  
+        })}
+      </section>
+    ) : <section className='no-results'>
+      <div className='no-results__text'>
+        <p> no results</p>
+      </div>
+    </section>
+  }
+
+  return renderSearchResults();
+  
+  
+}
+
+export default SearchPage;
