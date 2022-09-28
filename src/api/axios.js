93fe06a6982e023ea92a7051fe3f433f18b965ce
sally <peacheeks@gmail.com>
2022-09-28 12:40:27 +0900

    api key

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     params:{
-        api_key:'7bef7af82ec6f6830f6c7a686c98feb9',
+        api_key:process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: 'en-US',
     }
 });
